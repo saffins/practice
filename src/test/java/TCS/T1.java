73d99f6e15369f18d5a6a35b0205cafd5b652697
Nabu <saffin7867@gmail.com>
2019-10-19 19:24:02 +0530

    first

---
+++ src/test/java/TCS/T1.java
@@ -0,0 +1,19 @@
+package TCS;
+
+
+import org.testng.annotations.Test;
+
+
+public class T1 {
+
+	@Test
+	public void m1(){
+		System.out.println("m1");
+	}
+	
+	@Test
+	public void m2(){
+		System.out.println("m2");
+	}
+
+}
---
+++ src/test/java/TCS/T1Test.java
@@ -0,0 +1,11 @@
+package TCS;
+
+import org.testng.annotations.Test;
+
+public class T1Test {
+
+  @Test
+  public void m1() {
+    throw new RuntimeException("Test not implemented");
+  }
+}
